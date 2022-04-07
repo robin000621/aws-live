b3654811ed21b7e21335ac2b376cb6737c516665
robin000621 <78711716+robin000621@users.noreply.github.com>
2022-04-07 14:41:28 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cgaao7yemqk4.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tankianyangrobin-bucket"
+customregion = "us-east-1"
